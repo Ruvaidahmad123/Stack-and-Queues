e31ca30225a24e4f6f54824e07cd970a24b5185e
Ruvaid Ahmad <118987852+Ruvaidahmad123@users.noreply.github.com>
2024-09-29 14:35:06 +0530

    Time: 40 ms (25.85%) | Memory: 45.7 MB (30.11%) - LeetSync

---
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String removeDuplicates(String s) {
+        Stack<Character>st=new Stack<>();
+        for(int i=0;i<s.length();i++){
+            if (!st.isEmpty() && st.peek().equals(s.charAt(i))) {
+                st.pop();  
+            } else {
+                st.push(s.charAt(i));  
+            }
+        }
+        StringBuilder ans=new StringBuilder();
+        while(!st.isEmpty()){
+            ans.append(st.pop());
+        }
+        return ans.reverse().toString();
+    }
+}
