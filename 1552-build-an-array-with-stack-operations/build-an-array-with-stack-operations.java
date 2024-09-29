453de5ca5ead733d3a50a31c93b9fa0d570418f8
Ruvaid Ahmad <118987852+Ruvaidahmad123@users.noreply.github.com>
2024-09-29 13:29:10 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (86.32%) - LeetSync

--- 1552-build-an-array-with-stack-operations/build-an-array-with-stack-operations.java
+++ 1552-build-an-array-with-stack-operations/build-an-array-with-stack-operations.java
@@ -1,22 +1,24 @@
 class Solution {
     public List<String> buildArray(int[] target, int n) {
-        HashSet<Integer>set=new HashSet<>();
-        List<String>ans=new ArrayList<>();
-        int last=0;
-        for(int x:target){
-            set.add(x);
-            last=x;
-        }
-        for(int i=1;i<=n;i++){
-            if(set.contains(i)){
-                ans.add("Push");
-                if(i==last){
-                    break;
+        List<String> ans = new ArrayList<>();
+        for(int i=0; i<target.length; i++) {
+            if(i == 0) {
+                int timesPush = target[i] - 1;
+                for(int j=0; j<timesPush; j++) {
+                    ans.add("Push");
+                    ans.add("Pop");
                 }
-            }
-            else{
                 ans.add("Push");
-                ans.add("Pop");
+            }
+            else {
+                int diff = target[i] - target[i - 1];
+                int timesPush = diff - 1;
+                int timesPop = timesPush;
+                for(int j=0; j<timesPush; j++) {
+                    ans.add("Push");
+                    ans.add("Pop");
+                }
+                ans.add("Push");
             }
         }
         return ans;
