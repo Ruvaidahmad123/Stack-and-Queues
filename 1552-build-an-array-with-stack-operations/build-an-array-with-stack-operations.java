7868b599282a97093cbab1b2a3e5b4b1085f9ef0
Ruvaid Ahmad <118987852+Ruvaidahmad123@users.noreply.github.com>
2024-09-29 13:34:47 +0530

    Update build-an-array-with-stack-operations.java

--- 1552-build-an-array-with-stack-operations/build-an-array-with-stack-operations.java
+++ 1552-build-an-array-with-stack-operations/build-an-array-with-stack-operations.java
@@ -1,3 +1,30 @@
+O(n) space
+class Solution {
+    public List<String> buildArray(int[] target, int n) {
+        HashSet<Integer>set=new HashSet<>();
+        List<String>ans=new ArrayList<>();
+        int last=0;
+        for(int x:target){
+            set.add(x);
+            last=x;
+        }
+        for(int i=1;i<=n;i++){
+            if(set.contains(i)){
+                ans.add("Push");
+                if(i==last){
+                    break;
+                }
+            }
+            else{
+                ans.add("Push");
+                ans.add("Pop");
+            }
+        }
+        return ans;
+    }
+}
+
+O(1) space
 class Solution {
     public List<String> buildArray(int[] target, int n) {
         List<String> ans = new ArrayList<>();
