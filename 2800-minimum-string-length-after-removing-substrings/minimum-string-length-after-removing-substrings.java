aca46848efc05d656c849210b4e5528798aaf030
Ruvaid Ahmad <118987852+Ruvaidahmad123@users.noreply.github.com>
2024-10-07 13:32:20 +0530

    Update minimum-string-length-after-removing-substrings.java

--- 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.java
+++ 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.java
@@ -1,3 +1,50 @@
+//Approach-1 (Brute Force)
+//T.C : O(n^2)
+//S.C : O(1)
+class Solution {
+    public int minLength(String s) {
+        while (s.contains("AB") || s.contains("CD")) {
+            if (s.contains("AB")) {
+                s = s.replaceFirst("AB", "");
+            } else if (s.contains("CD")) {
+                s = s.replaceFirst("CD", "");
+            }
+        }
+        return s.length();
+    }
+}
+
+//Approach-2 (Using stack)
+//T.C : O(n)
+//S.C : O(n)
+class Solution {
+    public int minLength(String s) {
+        Stack<Character> st = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char currentChar = s.charAt(i);
+
+            if (st.isEmpty()) {
+                st.push(currentChar);
+                continue;
+            }
+
+            if (currentChar == 'B' && st.peek() == 'A') {
+                st.pop();
+            } else if (currentChar == 'D' && st.peek() == 'C') {
+                st.pop();
+            } else {
+                st.push(currentChar);
+            }
+        }
+
+        return st.size();
+    }
+}
+
+//Approach-3 (Using 2 pointers = i : read, j ; write)
+//T.C : O(n)
+//S.C : O(1)
 class Solution {
     public int minLength(String s) {
         int n = s.length();
